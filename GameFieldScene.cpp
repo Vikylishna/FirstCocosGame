87e41d9836f0748b47199252af305115d0c4563e
Victoria Iskanderova <tori.1992@inbox.ru>
2015-09-11 15:57:00 +0300

    Add cat

--- GameFieldScene.cpp
+++ GameFieldScene.cpp
@@ -33,20 +33,22 @@ bool GameField::init()
 	sprite_background->setPosition(origin);// +Point(visibleSize.width / 2, visibleSize.height / 2));	// +Point(-80, 80));
 	addChild(sprite_background, 0);
 
-	Field field;		//!!!!!!!!!!!!!!!!!
+	// Игровое поле
+	Field field;
 
-	std::vector<std::string> filename(4);			//!!!!!!!!!!!!!!!!!!!!!!!!!!
+	//Вектор с именами файлов, из которых мы создаем спрайты.
+	std::vector<std::string> filename(4);
+	fill_arr_filename(filename);		// Заполняем его.
 
-	fill_arr_filename(filename);		// Вектор содержащий имена файлов, из которых мы создаем спрайты
-
+	//Длина и ширина одной плитки.
 	int rectWidth = visibleSize.width / field.get_n();	//Ширину делим на кол-во столбцов. x
 	int rectHeight = visibleSize.height / field.get_m();	//Длину делим на кол-во строк. y
 
 	// Заполняем поле игры плитками
-	for (int i = 0; i < field.get_m(); i++)		//Проходим все строки
+	for (int i = 0; i < field.get_m(); i++)		//Проходим строки
 	{
-		for (int j = 0; j < field.get_n(); j++)
-		{		//Проходимся по столбцам
+		for (int j = 0; j < field.get_n(); j++)		//Проходим столбцы
+		{
 			auto sprite1 = Sprite::create(filename[field.get_value(i, j)], Rect(0, 0, rectWidth - 2, rectHeight - 2));	//("mysprite.png", Rect(0,0,rectWidth,40))
 			sprite1->setAnchorPoint(Vec2(0, 0));
 			sprite1->setPosition(origin + Point(1, 1) + Point(rectWidth*j, rectHeight * i));		//(x - ширина, j - столбец;  y - высота, i - строка)
@@ -56,15 +58,16 @@ bool GameField::init()
 
 	// Добавим спрайт персонажа
 	auto cat_sprite = Sprite::create("cat.png");
+	//Изменим его: сделаем нужного зазмера.
 	double coeff1 = ((double)rectWidth * 0.75) / ((double)cat_sprite->getBoundingBox().size.width);		//getContentSize().height - то же самое
 	double coeff2 = ((double)rectWidth * 0.75) / ((double)cat_sprite->getBoundingBox().size.height);
 	double coeff = std::min(coeff1, coeff2);
 	//log("COEFF = %f", cat_sprite->getBoundingBox().size.width);
 	cat_sprite->setScale(coeff1);		//Определили и задали требуемый размер
-	cat_sprite->setAnchorPoint(Vec2(0, 0));
-	cat_sprite->setPosition(origin + Point(1, 1));// +Point(visibleSize.width / 2, visibleSize.height / 2));	// +Point(-80, 80));
+	cat_sprite->setAnchorPoint(Vec2(0.5, 0.5));
+	cat_sprite->setPosition(origin + Point(rectWidth / 2, rectHeight / 2));
+	cat_sprite->setTag(4);
 	addChild(cat_sprite, 12);
-	CCLOG("Preceding with blanks: %10d", 1977);
 
 	///////////////////////////////////////////////////////////////////
 	///
@@ -81,10 +84,10 @@ bool GameField::init()
 
 void GameField::onMouseUp(Event *event)
 {// Тут будет что-то другое. Я просто смотрю как оно работает.
-	Vec2 origin = Director::getInstance()->getVisibleOrigin();
-	auto cat_sprite = Sprite::create("cat.png");
-	cat_sprite->setScale(0.0);
-	cat_sprite->setAnchorPoint(Vec2(0, 0));
-	cat_sprite->setPosition(origin + Point(80, 80));// +Point(visibleSize.width / 2, visibleSize.height / 2));	// +Point(-80, 80));
-	addChild(cat_sprite, 12);
+
+	if (this->getChildByTag(4) != NULL)		//Если объект с тегом 4 существует, а это cat.
+		log("It is exist");
+	else
+		log("Cat don't exist");
+
 }
