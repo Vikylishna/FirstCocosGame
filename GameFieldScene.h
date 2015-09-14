657944acce996741252c0637b81da8d33b135618
Victoria Iskanderova <tori.1992@inbox.ru>
2015-09-14 20:23:22 +0300

    message

--- GameFieldScene.h
+++ GameFieldScene.h
@@ -9,6 +9,8 @@ class GameField : public cocos2d::Layer
 	Field gameField;	// Нужно ли сделать эту переменную глобальной/синглтоном? Либо оставить тут.
 	int tileWidth;
 	int tileHeight;
+	int coordsPreviousClickX;
+	int coordsPreviousClickY;
 	cocos2d::Vec2 origin;
 public:
 	static cocos2d::Scene* createScene();
